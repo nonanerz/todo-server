6f64ccfd7af03b3ada261599a2af4a6376a04e8e
Bulava Eduard <bulavaeduard@gmail.com>
2018-02-11 10:34:09 +0200

    add start script

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const host = 'localhost'
 
 module.exports = {
   port: process.env.port || process.env.PORT || '3005',
-  mongoUrl: process.env.MONGODB_URI || `mongodb://${host}/foods`
+  mongoUrl: process.env.MONGODB_URI || `mongodb://ds229438.mlab.com:29438/todos`
 }
